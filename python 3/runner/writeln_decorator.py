ad6e64c5b2cecdd26690483975800e0a99031ed3
gregmalcolm <greg@gregmalcolm.com>
2010-08-05 00:07:40 -0400

    Missed the writeln_decorator file while updating from hg

---
+++ python 3/runner/writeln_decorator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import os
+
+# Taken from legacy python unittest
+class WritelnDecorator:
+    """Used to decorate file-like objects with a handy 'writeln' method"""
+    def __init__(self,stream):
+        self.stream = stream
+
+    def __getattr__(self, attr):
+        return getattr(self.stream,attr)
+
+    def writeln(self, arg=None):
+        if arg: self.write(arg)
+        self.write('\n') # text-mode streams translate to \r\n if needed
+
