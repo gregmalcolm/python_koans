a45f54b6b8aaaefef5948a1ea034025daaae3e20
gregmalcolm <greg@gregmalcolm.com>
2010-09-08 19:35:18 -0400

    Applied patch from tshepang to fix issue 14 in bitbucket tracker: Added tuple tests

--- python 2/koans/about_true_and_false.py
+++ python 2/koans/about_true_and_false.py
@@ -24,6 +24,7 @@ class AboutTrueAndFalse(Koan):
 
     def test_empty_collections_are_treated_as_false(self):
         self.assertEqual(__, self.truth_value([]))
+        self.assertEqual(__, self.truth_value(()))
         self.assertEqual(__, self.truth_value({}))
         self.assertEqual(__, self.truth_value(set()))
 
@@ -32,5 +33,6 @@ class AboutTrueAndFalse(Koan):
         
     def test_everything_else_is_treated_as_true(self):
         self.assertEqual(__, self.truth_value(1))
+        self.assertEqual(__, self.truth_value(1,))
         self.assertEqual(__, self.truth_value("Python is named after Monty Python"))
         self.assertEqual(__, self.truth_value(' '))
