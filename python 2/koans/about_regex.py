ca5edfdce973aa83a791d3b7f4b0396c7a6ecc5b
flopezluis <hola@flopezluis.es>
2011-01-20 12:31:04 +0100

    added one case

--- python 2/koans/about_regex.py
+++ python 2/koans/about_regex.py
@@ -34,7 +34,6 @@ class AboutRegex(Koan):
         m = re.match('Felix', string) #TIP: Maybe match it's not the best option
         self.assertEqual(len(m),2, "I want to know how many times appears my name")
 
-
     def test_matching_any_character(self):
         """
             Lesson 1 Matching any character
@@ -49,9 +48,8 @@ class AboutRegex(Koan):
                 + "sa1.xls"
 
         #TIP: remember the issue of this lesson
-        self.assertEquals(len(re.findall("a", string)),3, "I want to find all files for North America(na) or South America(sa)")
+        self.assertEquals(len(re.findall(__, string)),3, "I want to find all files for North America(na) or South America(sa)")
 
-        
     def test_matching_special_character(self):
         """
             Lesson 1 Matching special character
@@ -68,4 +66,23 @@ class AboutRegex(Koan):
                 + "sa1.xls"  
         #TIP you can use the pattern .a. which matches in above test but in this case matches more than you want
         self.assertEquals(len(re.findall(".a.", string)),3, "I want to find all files for North America(na) or South America(sa)")
+
+    def test_matching_set_character(self):
+        """
+            Lesson 1 Matching sets of characters
+
+            A set of characters is defined using the metacharacters [ and ]. Everything between them is part of the set and
+            any one of the set members must match (but not all).
+        """
+        string = "sales.xlx\n"    \
+                + "sales1.xls\n"  \
+                + "orders3.xls\n" \
+                + "apac1.xls\n" \
+                + "sales2.xls\n"  \
+                + "na1.xls\n"  \
+                + "na2.xls\n"  \
+                + "sa1.xls\n"  \
+                + "ca1.xls"  
+        #TIP you can use the pattern .a. which matches in above test but in this case matches more than you want
+        self.assertEquals(len(re.findall(".a.", string)),3, "I want to find all files for North America(na) or South America(sa), but not (ca)")
 
