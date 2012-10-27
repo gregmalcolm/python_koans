f3d83ba2c86675065b9d06b356b481a18882e3c0
Greg Malcolm <greg@gregmalcolm.com>
2012-10-27 17:57:51 -0400

    Quote fix py3 side of house

--- python3/koans/about_sets.py
+++ python3/koans/about_sets.py
@@ -6,7 +6,7 @@ from runner.koan import *
 class AboutSets(Koan):
     def test_sets_make_keep_lists_unique(self):
         highlanders = ['MacLeod', 'Ramirez', 'MacLeod', 'Matunas', 'MacLeod', 'Malcolm', 'MacLeod']
-        
+
         there_can_only_be_only_one = set(highlanders)
 
         self.assertEqual(__, there_can_only_be_only_one)
@@ -20,7 +20,7 @@ class AboutSets(Koan):
 
         self.assertEqual(__, type({1, 2, 3}).__name__)
         self.assertEqual(__, type({'one': 1, 'two': 2}).__name__)
-        
+
         self.assertEqual(__, type({}).__name__)
 
     def test_creating_sets_using_strings(self):
@@ -29,12 +29,12 @@ class AboutSets(Koan):
 
     def test_convert_the_set_into_a_list_to_sort_it(self):
         self.assertEqual(__, sorted(set('12345')))
-        
+
     # ------------------------------------------------------------------
 
     def test_set_have_arithmetic_operators(self):
-        scotsmen = set(['MacLeod', "Wallace', 'Willie'])
-        warriors = set(['MacLeod', "Wallace', 'Leonidas'])
+        scotsmen = set(['MacLeod', 'Wallace', 'Willie'])
+        warriors = set(['MacLeod', 'Wallace', 'Leonidas'])
 
         self.assertEqual(__, scotsmen - warriors)
         self.assertEqual(__, scotsmen | warriors)
@@ -46,9 +46,9 @@ class AboutSets(Koan):
     def test_we_can_query_set_membership(self):
         self.assertEqual(__, 127 in {127, 0, 0, 1} )
         self.assertEqual(__, 'cow' not in set('apocalypse now') )
-        
+
     def test_we_can_compare_subsets(self):
         self.assertEqual(__, set('cake') <= set('cherry cake'))
         self.assertEqual(__, set('cake').issubset(set('cherry cake')) )
-        
+
         self.assertEqual(__, set('cake') > set('pie'))
