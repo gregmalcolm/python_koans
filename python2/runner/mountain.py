158b936bf240491b9a4b27bed1722a3a7b844cbf
gregmalcolm <greg@gregmalcolm.com>
2013-02-15 07:24:59 -0500

    Fixed running of contemplate_koans with 0 arguments. Made it so you multiple arguments run as one suite of tests

--- python2/runner/mountain.py
+++ python2/runner/mountain.py
@@ -13,14 +13,14 @@ class Mountain:
         self.stream = WritelnDecorator(sys.stdout)
         self.tests = path_to_enlightenment.koans()
         self.lesson = Sensei(self.stream)
-    
+
     def walk_the_path(self, args=None):
         "Run the koans tests with a custom runner output."
-        
+
         if args and len(args) >=2:
-	    args.pop(0)
-	    for arg in args:
-            	self.tests = unittest.TestLoader().loadTestsFromName("koans." + arg)
-        	self.tests(self.lesson)
-        	self.lesson.learn()
+            args.pop(0)
+            test_names = ["koans." + test_name for test_name in args]
+            self.tests = unittest.TestLoader().loadTestsFromNames(test_names)
+        self.tests(self.lesson)
+        self.lesson.learn()
         return self.lesson
