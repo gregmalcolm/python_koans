92d996777eb3973ef5a4dd3f26d185d518778b72
Shishir Kakaraddi <shishirkakaraddi@gmail.com>
2013-08-03 18:26:12 -0700

    Replace a simple method in the test suite with a new test to test escaping quotes in a triple quoted string

--- python2/koans/about_strings.py
+++ python2/koans/about_strings.py
@@ -56,8 +56,9 @@ world!
         b = """Hello "world"."""
         self.assertEqual(__, (a == b))
 
-    def but_quotes_at_the_end_of_a_triple_quoted_string_are_still_tricky(self):
+    def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
         string = """Hello "world\""""
+        self.assertEqual(__, string)
 
     def test_plus_concatenates_strings(self):
         string = "Hello, " + "world"
