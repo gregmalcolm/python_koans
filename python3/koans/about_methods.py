1c590d0cceba354b53fe03ee511619ef94a2212d
kelleydv <kelleydv@gmail.com>
2014-02-06 06:33:00 -0800

    removed method arguments

--- python3/koans/about_methods.py
+++ python3/koans/about_methods.py
@@ -40,7 +40,7 @@ class AboutMethods(Koan):
     def method_which_returns(self, a, b):
         return a*b
 
-    def test_that_which_returns(self, a, b):
+    def test_that_which_returns(self):
         self.assertEqual(__, self.method_which_returns(3,4))
         # Notice that methods accessed from class scope do not require
         # you to pass the first "self" argument?
