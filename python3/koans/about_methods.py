f5bb1006eb3b3ec50ec76171d212dd6979cccb5f
kelleydv <kelleydv@gmail.com>
2014-02-05 19:53:38 -0800

    removed comments that had been copied

--- python3/koans/about_methods.py
+++ python3/koans/about_methods.py
@@ -52,8 +52,6 @@ class AboutMethods(Koan):
 
     def test_which_does_not_return_anything(self):
         self.assertEqual(__, self.pointless_method(1, 2))
-        # Notice that methods accessed from class scope do not require
-        # you to pass the first "self" argument?
 
     # ------------------------------------------------------------------
 
