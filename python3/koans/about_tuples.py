c40491c67cfef1c6b74ddacf7c85fe2ff52ae572
Daniel Karp <danielkarp@gmail.com>
2014-02-08 15:19:27 -0500

    Fix tuple creation koan

--- python3/koans/about_tuples.py
+++ python3/koans/about_tuples.py
@@ -6,7 +6,7 @@ from runner.koan import *
 class AboutTuples(Koan):
     def test_creating_a_tuple(self):
         count_of_three =  (1, 2, 5)
-        self.assertEqual(5, __)
+        self.assertEqual(__, count_of_three[2])
 
     def test_tuples_are_immutable_so_item_assignment_is_not_possible(self):
         count_of_three =  (1, 2, 5)
