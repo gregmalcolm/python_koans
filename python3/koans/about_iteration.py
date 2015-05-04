4f6d0277504c8d681e7aa8fa58697a7017787992
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-05-03 21:58:04 -0500

    use with for file opening in about_iteration.py to allow for automatic closing.

--- python3/koans/about_iteration.py
+++ python3/koans/about_iteration.py
@@ -121,17 +121,11 @@ class AboutIteration(Koan):
         self.assertEqual(__, list(result))
 
         try:
-            file = open("example_file.txt")
-
-            try:
+            with open("example_file.txt") as file:
                 def make_upcase(line):
                     return line.strip().upper()
                 upcase_lines = map(make_upcase, file.readlines())
                 self.assertEqual(__, list(upcase_lines))
-            finally:
-                # Arg, this is ugly.
-                # We will figure out how to fix this later.
-                file.close()
         except IOError:
             # should never happen
             self.fail()
