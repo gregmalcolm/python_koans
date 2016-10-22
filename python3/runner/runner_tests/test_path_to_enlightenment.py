60e63ba3f88445daf4f4d48c573af31857ed6858
Kevin J. Chase <kjc@greyeminence.org>
2016-10-22 12:21:49 -0400

    test_path_to_enlightenment: Safe for Python 3.1.
    The Unicode literal syntax requires Python 3.3+.
    
    TestCase.assertIsInstance requires Python 3.2+.

--- python3/runner/runner_tests/test_path_to_enlightenment.py
+++ python3/runner/runner_tests/test_path_to_enlightenment.py
@@ -10,7 +10,7 @@ from runner import path_to_enlightenment as pte
 class TestFilterKoanNames(unittest.TestCase):
 
     def test_empty_input_produces_empty_output(self):
-        infile = io.StringIO(u'')
+        infile = io.StringIO('')
         expected = []
         received = list(pte.filter_koan_names(infile))
         self.assertListEqual(expected, received)
@@ -18,27 +18,27 @@ class TestFilterKoanNames(unittest.TestCase):
 
     def test_names_yielded_match_names_in_file(self):
         names = [
-          u'this.is.a.test',
-          u'this.is.only.a.test',
+          'this.is.a.test',
+          'this.is.only.a.test',
           ]
-        infile = io.StringIO(u'\n'.join(names))
+        infile = io.StringIO('\n'.join(names))
         received = list(pte.filter_koan_names(infile))
         self.assertListEqual(names, received)
         return
 
     def test_whitespace_is_stripped(self):
         names = [
-          u'this.is.a.test',
-          u'    white.space.should.be.stripped',
-          u'this.is.only.a.test',
-          u'white.space.should.be.stripped    ',
+          'this.is.a.test',
+          '    white.space.should.be.stripped',
+          'this.is.only.a.test',
+          'white.space.should.be.stripped    ',
           ]
-        infile = io.StringIO(u'\n'.join(names))
+        infile = io.StringIO('\n'.join(names))
         expected = [
-          u'this.is.a.test',
-          u'white.space.should.be.stripped',
-          u'this.is.only.a.test',
-          u'white.space.should.be.stripped',
+          'this.is.a.test',
+          'white.space.should.be.stripped',
+          'this.is.only.a.test',
+          'white.space.should.be.stripped',
           ]
         received = list(pte.filter_koan_names(infile))
         self.assertListEqual(expected, received)
@@ -46,15 +46,15 @@ class TestFilterKoanNames(unittest.TestCase):
 
     def test_commented_out_names_are_excluded(self):
         names = [
-          u'this.is.a.test',
-          u'#this.is.a.comment',
-          u'this.is.only.a.test',
-          u'    #    this.is.also a.comment    ',
+          'this.is.a.test',
+          '#this.is.a.comment',
+          'this.is.only.a.test',
+          '    #    this.is.also a.comment    ',
           ]
-        infile = io.StringIO(u'\n'.join(names))
+        infile = io.StringIO('\n'.join(names))
         expected = [
-          u'this.is.a.test',
-          u'this.is.only.a.test',
+          'this.is.a.test',
+          'this.is.only.a.test',
           ]
         received = list(pte.filter_koan_names(infile))
         self.assertListEqual(expected, received)
@@ -62,12 +62,12 @@ class TestFilterKoanNames(unittest.TestCase):
 
     def all_blank_or_comment_lines_produce_empty_output(self):
         names = [
-          u' ',
-          u'# This is a comment.',
-          u'\t',
-          u'    # This is also a comment.',
+          ' ',
+          '# This is a comment.',
+          '\t',
+          '    # This is also a comment.',
           ]
-        infile = io.StringIO(u'\n'.join(names))
+        infile = io.StringIO('\n'.join(names))
         expected = []
         received = list(pte.filter_koan_names(infile))
         self.assertListEqual(expected, received)
@@ -79,7 +79,7 @@ class TestKoansSuite(unittest.TestCase):
     def test_empty_input_produces_empty_testsuite(self):
         names = []
         suite = pte.koans_suite(names)
-        self.assertIsInstance(suite, unittest.TestSuite)
+        self.assertTrue(isinstance(suite, unittest.TestSuite))
         expected = []
         received = list(suite)
         self.assertListEqual(expected, received)
@@ -92,7 +92,7 @@ class TestKoansSuite(unittest.TestCase):
           'koans.about_strings.AboutStrings',
           ]
         suite = pte.koans_suite(names)
-        self.assertIsInstance(suite, unittest.TestSuite)
+        self.assertTrue(isinstance(suite, unittest.TestSuite))
         expected = [
           'AboutAsserts',
           'AboutNone',
