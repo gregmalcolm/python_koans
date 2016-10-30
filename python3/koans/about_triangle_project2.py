91bc3c5cffd203b2ebbf0f2dd0d9753f45c63994
alexv86 <alex@alexvy86.com>
2016-10-30 12:46:30 -0500

    Explain errors to be handled in TriangleProject2

--- python3/koans/about_triangle_project2.py
+++ python3/koans/about_triangle_project2.py
@@ -10,15 +10,15 @@ class AboutTriangleProject2(Koan):
     # The first assignment did not talk about how to handle errors.
     # Let's handle that part now.
     def test_illegal_triangles_throw_exceptions(self):
+        # All sides should be greater than 0
         with self.assertRaises(TriangleError):
             triangle(0, 0, 0)
-
         with self.assertRaises(TriangleError):
             triangle(3, 4, -5)
 
+        # The sum of any two sides should be greater than the third one
         with self.assertRaises(TriangleError):
             triangle(1, 1, 3)
-
         with self.assertRaises(TriangleError):
             triangle(2, 5, 2)
 
