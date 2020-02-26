14df7627549ee465b5bd42ab0e4ee696c0e65aad
SuzannAdams <suzannyee@yahoo.com>
2020-02-25 21:42:26 -0800

    completed task 3, started task 4

--- python3/koans/about_list_assignments.py
+++ python3/koans/about_list_assignments.py
@@ -10,12 +10,12 @@ from runner.koan import *
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
         names = ["John", "Smith"]
-        self.assertEqual(__, names)
+        self.assertEqual(["John", "Smith"], names)
 
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual("John", first_name)
+        self.assertEqual("Smith", last_name)
 
     def test_parallel_assignments_with_extra_values(self):
         title, *first_names, last_name = ["Sir", "Ricky", "Bobby", "Worthington"]
