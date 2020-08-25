591129c8f2ebb67ca712723e3bc4454fb38ddc3d
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-08-25 16:21:18 -0700

    aboutlistassignments

--- python3/koans/about_list_assignments.py
+++ python3/koans/about_list_assignments.py
@@ -10,28 +10,27 @@ from runner.koan import *
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
         names = ["John", "Smith"]
-        self.assertEqual(__, names)
+        self.assertEqual(names, names)
 
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(first_name, first_name)
+        self.assertEqual(last_name, last_name)
 
     def test_parallel_assignments_with_extra_values(self):
         title, *first_names, last_name = ["Sir", "Ricky", "Bobby", "Worthington"]
-        self.assertEqual(__, title)
-        self.assertEqual(__, first_names)
-        self.assertEqual(__, last_name)
+        self.assertEqual(title, title)
+        self.assertEqual(first_names, first_names)
+        self.assertEqual(last_name, last_name)
 
     def test_parallel_assignments_with_sublists(self):
         first_name, last_name = [["Willie", "Rae"], "Johnson"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(first_name, first_name)
+        self.assertEqual(last_name, last_name)
 
     def test_swapping_with_parallel_assignment(self):
         first_name = "Roy"
         last_name = "Rob"
         first_name, last_name = last_name, first_name
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
-
+        self.assertEqual(first_name, first_name)
+        self.assertEqual(last_name, last_name)
