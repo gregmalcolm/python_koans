cf1f47da8e90edefa6b44a7c1efa2ce1b8e23b13
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-01-02 18:08:49 +0200

    Fix PEP 8: E266 too many leading '#' for block comment

--- koans/about_with_statements.py
+++ koans/about_with_statements.py
@@ -44,28 +44,28 @@ class AboutWithStatements(Koan):
     def test_finding_lines(self):
         self.assertEqual(__, self.find_line("example_file.txt"))
 
-    ## ------------------------------------------------------------------
-    ## THINK ABOUT IT:
-    ##
-    ## The count_lines and find_line are similar, and yet different.
-    ## They both follow the pattern of "sandwich code".
-    ##
-    ## Sandwich code is code that comes in three parts: (1) the top slice
-    ## of bread, (2) the meat, and (3) the bottom slice of bread.
-    ## The bread part of the sandwich almost always goes together, but
-    ## the meat part changes all the time.
-    ##
-    ## Because the changing part of the sandwich code is in the middle,
-    ## abstracting the top and bottom bread slices to a library can be
-    ## difficult in many languages.
-    ##
-    ## (Aside for C++ programmers: The idiom of capturing allocated
-    ## pointers in a smart pointer constructor is an attempt to deal with
-    ## the problem of sandwich code for resource allocation.)
-    ##
-    ## Python solves the problem using Context Managers. Consider the
-    ## following code:
-    ##
+    # ------------------------------------------------------------------
+    # THINK ABOUT IT:
+    #
+    # The count_lines and find_line are similar, and yet different.
+    # They both follow the pattern of "sandwich code".
+    #
+    # Sandwich code is code that comes in three parts: (1) the top slice
+    # of bread, (2) the meat, and (3) the bottom slice of bread.
+    # The bread part of the sandwich almost always goes together, but
+    # the meat part changes all the time.
+    #
+    # Because the changing part of the sandwich code is in the middle,
+    # abstracting the top and bottom bread slices to a library can be
+    # difficult in many languages.
+    #
+    # (Aside for C++ programmers: The idiom of capturing allocated
+    # pointers in a smart pointer constructor is an attempt to deal with
+    # the problem of sandwich code for resource allocation.)
+    #
+    # Python solves the problem using Context Managers. Consider the
+    # following code:
+    #
 
     class FileContextManager():
         def __init__(self, file_name):
