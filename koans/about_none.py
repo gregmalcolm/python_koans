ff7d8b730c8d8ae0dd7a720fffa1e829e1f27231
erika helm <erikadphelm16@gmail.com>
2023-03-03 09:28:30 -0600

    rising lab

--- koans/about_none.py
+++ koans/about_none.py
@@ -11,11 +11,11 @@ class AboutNone(Koan):
 
     def test_none_is_an_object(self):
         "Unlike NULL in a lot of languages"
-        self.assertEqual(__, isinstance(None, object))
+        self.assertEqual(True, isinstance(None, object))
 
     def test_none_is_universal(self):
         "There is only one None"
-        self.assertEqual(____, None is None)
+        self.assertEqual(True, None is None)
 
     def test_what_exception_do_you_get_when_calling_nonexistent_methods(self):
         """
